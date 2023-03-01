d73ff0cda3da64a43f3d035f1d6e59ee76e7ddbc
brian <egbujiebc@gmail.com>
2023-03-01 14:23:35 -0330

    boss.tf
    assignment

--- boss.tf
+++ boss.tf
@@ -1,1 +1,32 @@
-1
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "gitvpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "gitvpc"
+  }
+}
+
+resource "aws_subnet" "pubsubnetgit" {
+  vpc_id     = aws_vpc.gitvpc.id
+  cidr_block = "10.0.4.0/24"
+
+  tags = {
+    Name = "pubsubnetgit"
+  }
+}
+
